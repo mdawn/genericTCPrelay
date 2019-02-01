64e96ed2e4f815853abf0daa4bf82316c25f6962
Misty Dawn Hall <mistydawncarmichael@gmail.com>
2019-02-01 00:39:22 -0500

    relay supports multiple clients

--- relay.go
+++ relay.go
@@ -4,35 +4,14 @@ import (
 	"bufio"
 	"fmt"
 	"net"
-	"os"
 )
 
-func handleError(err error, msg string) {
-	if err != nil {
-		fmt.Printf("Error: %s : %s\n", msg, err.Error())
-		os.Exit(1)
-	}
-}
+func handleConnection(c net.Conn, e net.Conn) {
 
-func main() {
-	fmt.Println("relay runs")
-
-	var echoServerURL = "localhost:8080"
-	e, _ := net.Dial("tcp", echoServerURL)
-
-	port := "8081"
-
-	l, err := net.Listen("tcp", ":"+port)
-	handleError(err, "Listen()")
-
-	defer l.Close()
-	fmt.Println("Listening on Port", port)
-	c, err := l.Accept()
-	handleError(err, "Accept()")
+	reader := bufio.NewReader(c)
 
 	defer c.Close()
 	fmt.Printf("Connected to : %v\n", c.RemoteAddr())
-	reader := bufio.NewReader(c)
 
 	for {
 		text, _ := reader.ReadString('\n')
@@ -42,8 +21,29 @@ func main() {
 		m, _ := bufio.NewReader(e).ReadString('\n')
 		fmt.Print("Message from server: " + m)
 		// passes msg along
-		_, err = fmt.Fprintf(c, "%s\r\n", m)
-		handleError(err, "Fprintf()")
+		fmt.Fprintf(c, "%s\r\n", m)
+	}
+}
+
+func main() {
+
+	fmt.Println("relay runs")
+
+	var echoServerURL = "localhost:8080"
+
+	port := "8081"
+	fmt.Println("Listening on Port", port)
+
+	e, _ := net.Dial("tcp", echoServerURL)
+	l, _ := net.Listen("tcp", ":"+port)
+
+	for {
+		c, err := l.Accept()
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		go handleConnection(c, e)
 	}
 
 }
